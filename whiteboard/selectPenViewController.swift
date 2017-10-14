aa6b93fba717aa5c731ced7caf23841f9c49a2d9
dalianmengzi <dalianmengzi@users.noreply.github.com>
2017-10-14 23:19:40 +0800

    fixed
    笔画处理

--- whiteboard/selectPenViewController.swift
+++ whiteboard/selectPenViewController.swift
@@ -25,11 +25,31 @@ class selectPenViewController: UIViewController {
     }
     
     func setUI(){
-        let setView = UIView.init(frame: CGRect.init(x: width - 200, y: 30, width: 200, height: 60));
+        let setView = UIView.init(frame: CGRect.init(x: width - 150, y: 30, width: 150, height: 60));
         self.view.addSubview(setView);
+        let BtnW = 30 , colors = [UIColor.red, UIColor.black,UIColor.yellow,UIColor.purple,UIColor.orange];
         
-        
-        
+        for i in 0 ..< colors.count {
+            let button = UIButton()
+            button.tag = i
+            button.frame = CGRect(x:BtnW * i, y: 2, width: 16, height: 16);
+            button.layer.borderWidth = 0;
+            button.layer.cornerRadius = 8;
+            button.addTarget(self, action: #selector(self.selectColor(_:)), for: .touchUpInside)
+            button.backgroundColor = colors[i]
+            setView.addSubview(button)
+        }
+        for i in 0 ..< 5 {
+            let button = UIButton()
+            let penwidth = 8 + i * 2;
+            button.tag = penwidth / 2
+            button.frame = CGRect(x:BtnW * i + 4, y: 30, width: penwidth , height: penwidth);
+            button.layer.borderWidth = 0;
+            button.layer.cornerRadius = CGFloat(penwidth / 2);
+            button.addTarget(self, action: #selector(self.selectWidth(_:)), for: .touchUpInside)
+            button.backgroundColor = UIColor.red;
+            setView.addSubview(button)
+        }
        
     }
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -37,6 +57,12 @@ class selectPenViewController: UIViewController {
         
     }
 
+    func selectColor(_ button: UIButton) {
+        
+    }
+    func selectWidth(_ button: UIButton) {
+        
+    }
     /*
     // MARK: - Navigation
 
